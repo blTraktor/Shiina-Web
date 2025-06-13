2426fb233e23d865eb320b679e594dfd79e6de63
KozacheShapka <kozacheshapka@gmail.com>
2025-06-13 11:42:21 +0000

    Allow the use of russian nicknames. I don't know why, but they asked for it a long time ago

--- src/main/java/dev/osunolimits/routes/post/settings/customization/HandleNameChange.java
+++ src/main/java/dev/osunolimits/routes/post/settings/customization/HandleNameChange.java
@@ -36,7 +36,8 @@ public class HandleNameChange extends Shiina {
 
         String newName = req.queryParams("newname");
         String newSafeName = newName.toLowerCase().replaceAll(" ", "_");
-        if (newName == null || newName.isEmpty() || !newName.matches("^(?! )[\\w\\[\\] -]{2,15}(?<! )$")) {
+        if (newName == null || newName.isEmpty() ||
+            !newName.matches("^(?! )[\\w\\[\\] \\-а-яА-ЯёЁ]{2,15}(?<! )$")) {
             return redirect(res, shiina, "/settings?error=Invalid name");
         }
 
